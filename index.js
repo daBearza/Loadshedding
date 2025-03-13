a8f36cffeb430a6294207db15ef0ee0b5f8d1dcf
daBearza <cois.ger@gmail.com>
2025-03-13 16:59:49 +0200

    Updating ignore files and installing dotenv

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+const API_URL = "https://developer.sepush.co.za/business/2.0/"
+const api_key = process.env.API_KEY;
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+});
